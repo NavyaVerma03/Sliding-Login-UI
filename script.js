e19b129b9314231d9a1b8d32ecde9326417546f3
Navya Verma <navyaverma451@gmail.com>
2026-05-27 19:19:09 +0530

    Sliding Login UI

---
+++ script.js
@@ -0,0 +1,16 @@
+const container =
+document.querySelector(".container");
+
+const registerBtn =
+document.querySelector("#register");
+
+const loginBtn =
+document.querySelector("#login");
+
+registerBtn.onclick = () => {
+  container.classList.add("active");
+};
+
+loginBtn.onclick = () => {
+  container.classList.remove("active");
+};
